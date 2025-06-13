95f6c69199f1841f0349989e116fd58a497dad50
shivmatkawala <shivkumarmatkawala@gmail.com>
2025-06-13 10:03:39 +0530

    Added numeric dattatype file

---
+++ intdatatype.py
@@ -0,0 +1,102 @@
+# NUMERIC DATATYPES:-
+
+    # 1) INT Data Type:-
+        # An Integer is a number without decimal point.
+        # 0 is also an integer.
+        # +ve and -ve numbers can be Integers.
+    #EXAMPLES:-
+            # 1, 0, -45, 100, -509
+            
+    
+    # 2) Float Data Type:-
+        # A float is a number with decimal point.
+        # A float can be +ve and -ve.
+    #EXAMPLE:-
+        # 0.0, 34.5555550, 90.50, -1.456
+        
+    # 3) Complex Data Type:-
+        # A complex number is a combination of real number and imaginary number
+            # complex = real +/- imaginary
+            
+            # What is mean by real number:-
+                # All ints and floats are real numbers
+                    # Ex: 4, -5, 9.0, -23.55, 0.56
+            
+            #  What is imaginary number:-
+                # A number which comes with prefix 'j' is caalled imaginary number.
+                    #Ex: 3j, -8j, 90.56j, -12.23j
+                    
+        # EXAMPLES OF COMPLEX :-
+                    # 6 + 5j, -34 - 50j, 12 - 45j, 10.5 + 50.45j, -90.78 - 0.56j
+
+
+#################################################################
+
+
+# x = 5
+# y = -2
+
+# print(type(x))    #<class 'int'>
+# print(type(y))    #<class 'int'>
+
+# m =  45.50
+# n = -67.34
+# print(type(m)) #<class 'float'>
+# print(type(n)) #<class 'float'>
+
+
+# p = 5-7j  
+# q = -5.6 + 8.345j
+
+# print(type(p)) #<class 'complex'>
+# print(type(q)) #<class 'complex'>
+
+######################################
+
+# ADDITION +
+
+# num_1 = 15
+# num_2 = 5
+
+# sum = num_1 + num_2
+# print(sum)
+
+# SUTRACTION:-
+
+# sub = num_1 - num_2
+# print(sub)
+
+
+# MULTIPLICATION
+
+# mul = num_1 * num_2
+# print(mul)
+
+# s_div = num_1 / num_2
+# print(s_div)
+
+
+# print(33 / 15)
+
+# FLOOR DIVISION
+# print(33 // 15)
+
+# p = 7
+# q = 4
+
+# print(p % q)
+
+# print(45 % 20)
+
+# EXPONENT
+
+# print(3 ** 3)
+
+# print(5 ** 8)
+
+
+x = 56.5
+y = 13.2
+
+m = 24 + 6j
+n = 10+3j
