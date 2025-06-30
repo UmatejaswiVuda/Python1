330f02f340abcb08304d44fa20af5226c8b86b43
shivmatkawala <shivkumarmatkawala@gmail.com>
2025-06-30 10:12:17 +0530

    added test1, dictionary and set datatypes

---
+++ tes1-solutions.py
@@ -0,0 +1,19 @@
+# str1 = "iuhgSDFGHiuejfdSDX"
+# cap_string = str1[4:9:1] + str1[15::]
+# print(cap_string)
+
+# l1 =  [[12,  23, 34], ['A', 'C', 'J'], 'Apple', True]
+# [23, 'elppA', c]
+
+# new_list = list(l1[0][1]) + list(l1[2][::-1]) + list(l1[1][1])
+# print(new_list)
+
+# new_list = []
+# new_list.append(l1[0][1])
+# new_list.append(l1[2][::-1])
+# new_list.append(l1[1][1].lower())
+# print(new_list)
+
+###################################################
+
+
