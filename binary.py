ea8ee3351928b7de5fdddc057d1aac962e0bd5ed
shivmatkawala <shivkumarmatkawala@gmail.com>
2025-07-04 10:12:38 +0530

    added binary.py

---
+++ binary.py
@@ -0,0 +1,21 @@
+# decimmal_num = 0
+
+# binary_num = bin(decimmal_num)
+# print(binary_num)   # 0
+
+
+binary_num = '11111111'
+decimal_num = int(binary_num, 2)
+print(decimal_num)
+
+print(bin(65))
+
+print(ord('A'))   # 65
+
+print(ord('q'))  # 113
+
+print(ord('@'))   # 64
+
+print(ord('*'))   # 42
+
+
